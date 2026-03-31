97d69eebfa528b2b9e13eb3c35322c5b9a64ab2e
Jigar215 <prajapatijigar215@gmail.com>
2026-03-31 17:54:53 +0530

    Pushing the /view of the project

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 from fastapi import FastAPI
+import json
 
 app = FastAPI()
+
+def load_data():
+    with open('patients.json', 'r') as f:
+          data = json.load(f)
+
+    return data
 
 @app.get("/")
 def hello():
@@ -10,3 +17,7 @@ def hello():
 def about():
     return{'message': 'A fully functional API to manage your patient records'}
 
+@app.get("/view")
+def view():
+     data = load_data()
+     return data
